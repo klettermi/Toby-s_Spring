79daa2dd054775371fa8fe3c827c6530203a00e4
Mi <mi_aa_@naver.com>
2024-01-07 00:07:06 +0900

    feat: JDBC를 이용한 등록과 조회 기능이 있는 UserDao 클래스

---
+++ src/main/java/toby/user/dao/UserDao.java
@@ -0,0 +1,55 @@
+package toby.user.dao;
+
+import toby.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // 1. DB 연결을 위한 Connection을 가져온다.
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/springbook","root", "Cjssus97!!");
+
+        // 2. SQL을 담은 Statement(or PreparedStatement)를 만든다.
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values (?, ?, ?)"
+        );
+
+        // 3. ps를 실행한다.
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        // 4. 작업 중에 생성된 Connection, Statement, ResultSet과 같은 리소스는 작업이 마친 후 반드시 닫아준다.
+        ps.close();
+        c.close();;
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/springbook","root", "Cjssus97!!");
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?"
+        );
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}

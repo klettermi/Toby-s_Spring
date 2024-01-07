da6cc0b688a125c96916f22b5f4346591855b7fc
Mi <mi_aa_@naver.com>
2024-01-07 15:40:32 +0900

    feat: DataSource 타입의 datasource 빈 정의 메소드

--- src/main/java/toby/user/dao/DaoFactory.java
+++ src/main/java/toby/user/dao/DaoFactory.java
@@ -2,6 +2,9 @@ package toby.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정 정보라는 표시
 public class DaoFactory {
@@ -9,12 +12,19 @@ public class DaoFactory {
     @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
         return userDao;
     }
 
     @Bean
-    public ConnectionMaker connectionMaker(){
-        return new DConnectionMaker();
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springbook");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Cjssus97!!");
+
+        return dataSource;
     }
 }

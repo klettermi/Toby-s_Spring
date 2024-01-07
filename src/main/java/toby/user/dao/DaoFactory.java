cb3e7240b63feada58adb226bb24dc8e74444439
Mi <mi_aa_@naver.com>
2024-01-07 14:13:45 +0900

    feat: UserDao의 생성 책임을 맡은 팩토리 클래스

---
+++ src/main/java/toby/user/dao/DaoFactory.java
@@ -0,0 +1,9 @@
+package toby.user.dao;
+
+public class DaoFactory {
+    public UserDao userDao(){
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+    }
+}

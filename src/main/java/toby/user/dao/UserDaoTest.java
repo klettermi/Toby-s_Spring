444692c287fac3a2b375551c4a867c3f205d833d
Mi <mi_aa_@naver.com>
2024-01-07 13:55:48 +0900

    feat: 관계설정 책임이 추가된 UserDao 클라이언트인 main() 메소드

---
+++ src/main/java/toby/user/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package toby.user.dao;
+
+import toby.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao dao = new UserDao(connectionMaker);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}

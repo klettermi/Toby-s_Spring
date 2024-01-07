209ed3bd6f28554b6d9828e74081926f6022e232
Mi <mi_aa_@naver.com>
2024-01-07 15:52:20 +0900

    feat: datasource의 xml 프로퍼티 적용

--- src/main/java/toby/user/dao/CountingDaoFactory.java
+++ src/main/java/toby/user/dao/CountingDaoFactory.java
@@ -6,30 +6,22 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
 
-@Configuration
 public class CountingDaoFactory {
-    @Bean
+
     public UserDao userDao() {
         UserDao userDao = new UserDao();
         userDao.setDataSource(dataSource());
         return userDao;
     }
 
-    @Bean
+
     public ConnectionMaker connectionMaker(){
         return new CountingConnectionMaker(dataSource());
     }
 
-    @Bean
+
     public DataSource dataSource(){
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost:3306/springbook");
-        dataSource.setUsername("root");
-        dataSource.setPassword("Cjssus97!!");
-
-        return dataSource;
+        return new SimpleDriverDataSource();
     }
 
 
--- src/main/java/toby/user/dao/DaoFactory.java
+++ src/main/java/toby/user/dao/DaoFactory.java
@@ -1,30 +1,21 @@
 package toby.user.dao;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
 
-@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정 정보라는 표시
+ // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정 정보라는 표시
 public class DaoFactory {
 
-    @Bean
+
     public UserDao userDao() {
         UserDao userDao = new UserDao();
         userDao.setDataSource(dataSource());
         return userDao;
     }
 
-    @Bean
+
     public DataSource dataSource(){
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost:3306/springbook");
-        dataSource.setUsername("root");
-        dataSource.setPassword("Cjssus97!!");
-
-        return dataSource;
+        return new SimpleDriverDataSource();
     }
 }

4c2910f4641c46d3498e6467766b40d470737af9
Mi <mi_aa_@naver.com>
2024-01-07 13:47:55 +0900

    feat: ConnectionMaker 인터페이스

---
+++ src/main/java/toby/user/dao/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package toby.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException;
+}

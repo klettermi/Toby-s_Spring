ab911ed659cce05dddb3d1a02b924502cc2c346b
Mi <mi_aa_@naver.com>
2024-01-07 14:35:42 +0900

    feat: 스프링 빈 팩토리가 사용할 설정정보를 담은 DaoFactory 클래스

--- src/main/java/toby/user/dao/DaoFactory.java
+++ src/main/java/toby/user/dao/DaoFactory.java
@@ -1,10 +1,16 @@
 package toby.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정 정보라는 표시
 public class DaoFactory {
+    @Bean // 오브젝트 생성을 담당하는 IoC용 메서드라는 표시
     public UserDao userDao(){
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new DConnectionMaker();
     }

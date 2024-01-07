5becbb10e756aba49dd6c128189bb87c80a91417
Mi <mi_aa_@naver.com>
2024-01-07 15:50:42 +0900

    feat: XML 적용

--- src/main/java/toby/user/dao/UserDaoTest.java
+++ src/main/java/toby/user/dao/UserDaoTest.java
@@ -2,13 +2,14 @@ package toby.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import toby.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();

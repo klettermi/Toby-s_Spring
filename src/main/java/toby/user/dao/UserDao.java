d92623a4d1680736cdc5d23999b35708dbe3ee79
Mi <mi_aa_@naver.com>
2024-01-08 19:37:25 +0900

    feat: 변하는 부분을 메소드로 추출한 후의 deleteAll()

--- src/main/java/toby/user/dao/UserDao.java
+++ src/main/java/toby/user/dao/UserDao.java
@@ -66,7 +66,7 @@ public class UserDao {
 
         try{
             c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
+            ps = makeStatement(c);
             ps.executeUpdate();
         }catch(SQLException e){
             throw e;
@@ -87,6 +87,11 @@ public class UserDao {
         }
     }
 
+    private PreparedStatement makeStatement(Connection c) throws SQLException{
+        PreparedStatement ps;
+        ps = c.prepareStatement("delete from users");
+        return ps;
+    }
     public int getCount() throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;

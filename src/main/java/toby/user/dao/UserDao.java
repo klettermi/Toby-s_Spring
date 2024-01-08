5f27a3a883a925c0af85ebeeefc26fa1e68688c0
Mi <mi_aa_@naver.com>
2024-01-08 22:07:07 +0900

    feat: JdbcTemplate를 적용한 UserDao 클래스

--- src/main/java/toby/user/dao/UserDao.java
+++ src/main/java/toby/user/dao/UserDao.java
@@ -1,13 +1,12 @@
 package toby.user.dao;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
 import toby.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
@@ -16,6 +15,17 @@ public class UserDao {
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
 
     public void add(final User user) throws SQLException {
         this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
@@ -38,17 +48,6 @@ public class UserDao {
 
     public List<User> getAll(){
         return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
-    }
-
-    private RowMapper<User> userMapper = new RowMapper<User>() {
-        @Override
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-            return user;
-        }
     }
 }
 

c0ed55b81169d51c33e94968d9c9f93f8d20ab55
Mi <mi_aa_@naver.com>
2024-01-08 20:45:18 +0900

    feat: DI 적용을 위한 클라이언트/컨텍스트 분리

--- src/main/java/toby/user/dao/UserDao.java
+++ src/main/java/toby/user/dao/UserDao.java
@@ -61,32 +61,25 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
         Connection c = null;
         PreparedStatement ps = null;
 
         try{
             c = dataSource.getConnection();
 
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
+            ps = stmt.makePreparedStatement(c);
 
             ps.executeUpdate();
-        }catch(SQLException e){
+        }catch (SQLException e){
             throw e;
-        }finally {
-            if(ps != null){
-                try{
-                    ps.close();
-                }catch (SQLException e){
-                }
-            }
-            if(c != null){
-                try{
-                    c.close();
-                }catch (SQLException e){
-
-                }
-            }
+        }finally{
+            if(ps != null){ try{ ps.close();} catch (SQLException e){}}
+            if(c != null) {try{ c.close();} catch (SQLException e){}}
         }
     }
 

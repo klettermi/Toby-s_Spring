ce179b206bf465a29f52fe707fdcc85f0587eea4
Mi <mi_aa_@naver.com>
2024-01-08 22:00:23 +0900

    feat: 테스트 보완

--- src/test/java/toby/test/UserDaoTest.java
+++ src/test/java/toby/test/UserDaoTest.java
@@ -13,6 +13,7 @@ import toby.user.dao.UserDao;
 import toby.user.domain.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -74,5 +75,37 @@ public class UserDaoTest {
 
         Assertions.assertThrows(EmptyResultDataAccessException.class, () ->
                 dao.get("unknown_id"));
+    }
+
+    @Test
+    public void getAll() throws SQLException {
+        dao.deleteAll();
+
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size()).isEqualTo(0);
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size()).isEqualTo(1);
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size()).isEqualTo(2);
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size()).isEqualTo(3);
+        checkSameUser(user3, users3.get(0));
+        checkSameUser(user1, users3.get(1));
+        checkSameUser(user2, users3.get(2));
+    }
+
+    private void checkSameUser(User user1, User user2){
+        assertThat(user1.getId()).isEqualTo(user2.getId());
+        assertThat(user1.getName()).isEqualTo(user2.getName());
+        assertThat(user1.getPassword()).isEqualTo(user2.getPassword());
     }
 }

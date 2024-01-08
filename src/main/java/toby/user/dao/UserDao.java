422fcf16392019dc1a453c6d5d9b8bb977e175bf
Mi <mi_aa_@naver.com>
2024-01-08 20:51:04 +0900

    feat: user 정보를 AddStatement에 전달해주는 add() 메소드

--- src/main/java/toby/user/dao/UserDao.java
+++ src/main/java/toby/user/dao/UserDao.java
@@ -14,23 +14,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        // 2. SQL을 담은 Statement(or PreparedStatement)를 만든다.
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users(id, name, password) values (?, ?, ?)"
-        );
-
-        // 3. ps를 실행한다.
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        // 4. 작업 중에 생성된 Connection, Statement, ResultSet과 같은 리소스는 작업이 마친 후 반드시 닫아준다.
-        ps.close();
-        c.close();;
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {

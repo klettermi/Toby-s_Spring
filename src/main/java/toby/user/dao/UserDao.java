c3970620848922544874178e319ac6fa3cadc4f5
Mi <mi_aa_@naver.com>
2024-01-08 21:24:29 +0900

    feat: 변하지 않는 부분을 분리시킨 deleteAll()메소드

--- src/main/java/toby/user/dao/UserDao.java
+++ src/main/java/toby/user/dao/UserDao.java
@@ -62,31 +62,17 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy(){
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException{
-                    PreparedStatement ps = c.prepareStatement("delete from users");
-                    return ps;
-                }
-            }
-        );
+        executeSql("delete from users");
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try{
-            c = dataSource.getConnection();
-
-            ps = stmt.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        }catch (SQLException e){
-            throw e;
-        }finally{
-            if(ps != null){ try{ ps.close();} catch (SQLException e){}}
-            if(c != null) {try{ c.close();} catch (SQLException e){}}
-        }
+    private void executeSql(final String query) throws SQLException{
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy(){
+               public PreparedStatement makePreparedStatement(Connection c) throws SQLException{
+                   PreparedStatement ps = c.prepareStatement(query);
+                   return ps;
+               }
+           }
+        );
     }
 
     public int getCount() throws SQLException {

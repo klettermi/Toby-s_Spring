bf4a3c30349c454908c8215dcd26e508a03b7ab2
Mi <mi_aa_@naver.com>
2024-01-10 15:39:01 +0900

    feat: UserDaoTest 수정

--- src/test/java/toby/test/UserDaoTest.java
+++ src/test/java/toby/test/UserDaoTest.java
@@ -10,6 +10,8 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import toby.user.dao.UserDao;
+import toby.user.dao.UserDaoJdbc;
+import toby.user.domain.Level;
 import toby.user.domain.User;
 
 import java.sql.SQLException;
@@ -22,6 +24,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserDaoTest {
     @Autowired
     private ApplicationContext context;
+
+    @Autowired
     private UserDao dao;
     private User user1;
     private User user2;
@@ -29,11 +33,11 @@ public class UserDaoTest {
 
     @BeforeEach
     public void setUp(){
-        this.dao = context.getBean("userDao", UserDao.class);
+        this.dao = context.getBean("userDao", UserDaoJdbc.class);
 
-        this.user1 = new User("gyumee", "박성철", "springno1");
-        this.user2 = new User("leegw700", "이길원", "springno2");
-        this.user3 = new User("bumjin", "박범진", "springno3");
+        this.user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0);
+        this.user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 55, 10);
+        this.user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 40);
     }
     @Test
     public void addAndGet() throws SQLException {
@@ -107,5 +111,8 @@ public class UserDaoTest {
         assertThat(user1.getId()).isEqualTo(user2.getId());
         assertThat(user1.getName()).isEqualTo(user2.getName());
         assertThat(user1.getPassword()).isEqualTo(user2.getPassword());
+        assertThat(user1.getLevel()).isEqualTo(user2.getLevel());
+        assertThat(user1.getLogin()).isEqualTo(user2.getLogin());
+        assertThat(user1.getRecommend()).isEqualTo(user2.getRecommend());
     }
 }

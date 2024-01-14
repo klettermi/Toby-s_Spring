d93051d7c644388ab2ebb349c6e561383c8af5cc
Mi <mi_aa_@naver.com>
2024-01-15 00:11:53 +0900

    feat: 트랜잭션 코드를 제거한 UserService 구현 클래스

---
+++ src/main/java/toby/user/service/UserServiceImpl.java
@@ -0,0 +1,79 @@
+package toby.user.service;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import toby.user.dao.UserDao;
+import toby.user.domain.Level;
+import toby.user.domain.User;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class UserServiceImpl implements UserService{
+    UserDao userDao;
+    public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+    public static final int MIN_RECCOMEND_FOR_GOLD = 30;
+    private DataSource dataSource;
+    private PlatformTransactionManager transactionManager;
+    private MailSender mailSender;
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void add(User user){
+        if(user.getLevel() == null) user.setLevel(Level.BASIC);
+        userDao.add(user);
+    }
+
+    public void upgradeLevels(){
+        List<User> users = userDao.getAll();
+        for(User user : users){
+            if(canUpgradeLevel(user)){
+                upgradeLevel(user);
+            }
+        }
+    }
+
+
+    private boolean canUpgradeLevel(User user){
+        Level currentLevel = user.getLevel();
+        switch (currentLevel){
+            case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+            case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+            case GOLD: return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+        }
+    }
+
+    protected void upgradeLevel(User user){
+        user.upgradeLevel();
+        userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("useradmin@ksug.org");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+
+        this.mailSender.send(mailMessage);
+    }
+}
